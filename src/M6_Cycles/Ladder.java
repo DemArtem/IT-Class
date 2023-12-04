eb15a9b0f98b48ee40125cdc422c05e06f3ffb1a
gk <demar.demidov@yandex.ru>
2023-12-04 21:32:59 +0300

    intro

---
+++ src/M6_Cycles/Ladder.java
@@ -0,0 +1,24 @@
+package M6_Cycles;
+
+public class Ladder {
+    public static void main(String[] args) {
+
+        for (int i=1; i<=9; i++)
+            System.out.println("floor: "+i);
+
+        for (int i=0, j=0; i<16; i++) {
+            System.out.println("floor: " + i);
+            i+=j;j++;
+        }
+
+        for (int i=0; i<9; i++) {
+            if (i==4) continue;
+            System.out.println("floor: " + (i+1));
+        }
+
+        for (int i=0; i<9; i++) {
+            System.out.println("floor: " + (i+1));
+            if (i==4) break;
+        }
+    }
+}
