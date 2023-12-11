7183ab056ccdb7f28c59c9503776e057a6c33630
gk <demar.demidov@yandex.ru>
2023-12-11 21:29:19 +0300

    intro

---
+++ src/M8_CyclesFor_While/Fibonacchi.java
@@ -0,0 +1,15 @@
+package M8_CyclesFor_While;
+
+public class Fibonacchi {
+    public static void main(String[] args) {
+        int first=0; int second = 1;
+        int third;
+        for (int i=0; i<10; i++){
+            third = first+second;
+            System.out.print(third+" ");
+            first = second;
+            second = third;
+        }
+
+    }
+}
