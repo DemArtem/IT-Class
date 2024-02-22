1ebc3e983635ff597346a51c30688f101575a114
gk <demar.demidov@yandex.ru>
2024-02-22 20:39:48 +0300

    test

---
+++ src/IT_Class/M21_Swing/Sample2.java
@@ -0,0 +1,38 @@
+package IT_Class.M21_Swing;
+
+import javax.swing.*;
+
+public class Sample2  extends JFrame {
+    private JPanel panel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JButton button11;
+    private JButton button12;
+    private JButton button13;
+    private JButton button14;
+    private JButton button15;
+    private JButton button16;
+
+    public Sample2(){
+        setTitle("Sample2");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 300);
+        setContentPane(panel);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Sample2();
+    }
+}
+
+
