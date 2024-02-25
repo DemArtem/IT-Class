9c4bf947652366048b581c4a834215764e32fe5c
DemArtem <demar.demidov@yandex.ru>
2024-02-25 11:13:02 +0300

    test

---
+++ src/HomeWorks/HW13_Collections/HW_ArrayList.java
@@ -0,0 +1,39 @@
+package HomeWorks.HW13_Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+Реализовать вложенные структуры данных (на верхнем уровне коллекция коллекций, на среднем - коллекция и на нижнем - элементы типа String)
+с возможностью добавления, удаления и вывода элементов.
+Варианты:
+1. ArrayList<Stack>
+ */
+public class HW_ArrayList {
+    public static void main(String[] args) {
+        //коллекция коллекций(упаковка большой коробки)
+        List<Stack<String>> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Stack<String> stack = new Stack<>();
+            for (int j = 0; j < 5; j++) {
+                stack.push(String.format("string %d", i * 5 + j));
+            }
+            list.add(stack);
+        }
+
+        //изменение 2-ой строки в 1-й коробке
+        Stack<String> st = list.get(1);
+        st.set(2, "string selected");
+
+        //распаковка
+        for (int i = 0; i < list.size(); i++) {
+            Stack<String> stack = list.get(i);
+            while (!stack.empty()) {
+                String s = stack.pop();
+                System.out.print(s + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/HomeWorks/HW13_Collections/HW_PriorityQueue.java
@@ -0,0 +1,59 @@
+package HomeWorks.HW13_Collections;
+/*
+Реализовать вложенные структуры данных (на верхнем уровне коллекция коллекций, на среднем - коллекция и на нижнем - элементы типа String)
+с возможностью добавления, удаления и вывода элементов.
+Варианты:
+2. LinkedList<PriorityQueue>
+ */
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+public class HW_PriorityQueue {
+    private LinkedList<PriorityQueue<String>> nestedList;
+
+    public HW_PriorityQueue() {
+        nestedList = new LinkedList<>();
+    }
+
+    public void addElement(String element, int priority) {
+        if (nestedList.isEmpty() || nestedList.getLast().size() >= priority) {
+            nestedList.addLast(new PriorityQueue<>());
+        }
+        nestedList.getLast().add(element);
+    }
+
+    public void removeElement(String element) {
+        for (PriorityQueue<String> queue : nestedList) {
+            if (queue.contains(element)) {
+                queue.remove(element);
+                if (queue.isEmpty()) {
+                    nestedList.remove(queue);
+                }
+                break;
+            }
+        }
+    }
+
+    public void printElements() {
+        for (PriorityQueue<String> queue : nestedList) {
+            for (String element : queue) {
+                System.out.println(element);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        HW_PriorityQueue nestedData = new HW_PriorityQueue();
+        nestedData.addElement("Apple", 1);
+        nestedData.addElement("Banana", 2);
+        nestedData.addElement("Orange", 1);
+
+        System.out.println("Elements:");
+        nestedData.printElements();
+
+        nestedData.removeElement("Banana");
+
+        System.out.println("\nElements after removing 'Banana':");
+        nestedData.printElements();
+    }
+}
