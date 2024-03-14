f94cfecdfdc70be29cb9fa38b407979ac0cefbff
gk <demar.demidov@yandex.ru>
2024-03-14 20:27:06 +0300

    test

---
+++ src/IT_Class/M24_Patterns/Shopping.java
@@ -0,0 +1,60 @@
+package IT_Class.M24_Patterns;
+
+//decorator (поведенческий шаблон)
+public class Shopping {
+    public static void main(String[] args) {
+        Man man = new Dressmaker(new Shoemaker(new Pedestrian()));
+        man.walk();
+    }
+}
+
+interface Man{
+    void walk();
+}
+
+class Pedestrian implements Man{
+
+    @Override
+    public void walk() {
+        System.out.println("I go down the street");
+    }
+}
+
+abstract class Decorator implements Man{
+    protected Man man;
+
+    public Decorator(Man man) {
+
+        this.man = man;
+    }
+
+    @Override
+    public void walk() {
+        man.walk();
+    }
+}
+
+class Shoemaker extends Decorator{
+
+    public Shoemaker(Man man) {
+        super(man);
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("I made shoes for the man");
+        super.walk();
+    }
+}
+class Dressmaker extends Decorator{
+
+    public Dressmaker(Man man) {
+        super(man);
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("I made dress for the man");
+        super.walk();
+    }
+}
---
+++ src/IT_Class/M24_Patterns/SillyLittleMariam.java
@@ -0,0 +1,78 @@
+package IT_Class.M24_Patterns;
+
+//prototype (порождающий шаблон)
+public class SillyLittleMariam {
+    public static void main(String[] args) {
+        Girl mariam = new Girl("Mariam");
+        System.out.println(mariam.toString());
+
+        Girl noMariam = (Girl)mariam.clone();
+        noMariam.setName("No Mariam");
+        noMariam.setLazy(false);
+        noMariam.setSilly(false);
+        System.out.println(noMariam.toString());
+    }
+}
+
+class Girl implements Cloneable {
+    private String name = "Mariam";
+    private boolean silly = true;
+    private boolean little = true;
+    private boolean lazy = true;
+
+    public Girl(String name) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Girl clone() {
+        try {
+            Girl clone = (Girl) super.clone();
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Girl{" +
+                "name='" + name + '\'' +
+                ", silly=" + silly +
+                ", little=" + little +
+                ", lazy=" + lazy +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSilly() {
+        return silly;
+    }
+
+    public void setSilly(boolean silly) {
+        this.silly = silly;
+    }
+
+    public boolean isLittle() {
+        return little;
+    }
+
+    public void setLittle(boolean little) {
+        this.little = little;
+    }
+
+    public boolean isLazy() {
+        return lazy;
+    }
+
+    public void setLazy(boolean lazy) {
+        this.lazy = lazy;
+    }
+}
