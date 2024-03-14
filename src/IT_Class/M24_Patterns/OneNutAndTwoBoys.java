b524d0c76f2ef35235bfa6e2c84793456c5048a7
gk <demar.demidov@yandex.ru>
2024-03-14 19:25:15 +0300

    test

--- src/IT_Class/M24_Patterns/OneNutAndTwoBoys.java
+++ src/IT_Class/M24_Patterns/OneNutAndTwoBoys.java
@@ -1,8 +1,17 @@
 package IT_Class.M24_Patterns;
+
+import java.util.ArrayList;
+
 //Mediator Посредник
 public class OneNutAndTwoBoys {
     public static void main(String[] args) {
-
+        TallBoy mediator = new TallBoy();
+        Boy first = new LittleBoy(mediator);
+        Boy second = new LittleBoy(mediator);
+        mediator.addBoy(first);
+        mediator.addBoy(second);
+        first.request(5);
+        mediator.response();
     }
 }
 
@@ -25,3 +34,51 @@ abstract class Boy {
     public abstract void request(int count);
 }
 
+class TallBoy implements Mediator {
+
+    private final ArrayList<Boy> boys;
+    private int total = 0;
+
+    public TallBoy() {
+        boys = new ArrayList<>();
+    }
+
+    public void addBoy(Boy boy) {
+        boys.add(boy);
+    }
+
+    @Override
+    public void response() {
+        //let all other  know that this has changed
+        int countOfBoys = boys.size();
+        int count = total / countOfBoys;
+        int remainder = total % countOfBoys;
+        for (Boy currBoy : boys) {
+            //don't tell ourselves
+            currBoy.receive(count);
+        }
+        System.out.println("Mediator: Total count of nuts is " + total + ". " + remainder + " nuts is (are) mine.");
+    }
+
+    @Override
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
+
+class LittleBoy extends Boy {
+
+    public LittleBoy(Mediator mediator) {
+        super(mediator);
+    }
+
+    @Override
+    public void receive(int count) {
+        System.out.println("Boy: I got " + count + " nuts. ");
+    }
+
+    @Override
+    public void request(int count) {
+        mediator.setTotal(count);
+    }
+}
---
+++ src/IT_Class/M24_Patterns/RoosterAndBeanSeed.java
@@ -0,0 +1,60 @@
+package IT_Class.M24_Patterns;
+
+//chain of resposibility
+public class RoosterAndBeanSeed {
+    public static void main(String[] args) {
+        Hen hen = new Hen();
+        Cow cow = new Cow();
+        Mower mower = new Mower();
+        Stove stove = new Stove();
+        hen.setNext(cow);
+        cow.setNext(mower);
+        mower.setNext(stove);
+
+        hen.help();
+    }
+}
+
+abstract class Helper {
+    private Helper next;
+
+    public void setNext(Helper next) {
+        this.next = next;
+    }
+
+    public void help() {
+        sendMessage();
+        if (next != null)
+            next.help();
+    }
+
+    abstract void sendMessage();
+}
+
+class Hen extends Helper {
+    @Override
+    void sendMessage() {
+        System.out.println("Курица - корове: дай маслица.");
+    }
+}
+
+class Cow extends Helper {
+    @Override
+    void sendMessage() {
+        System.out.println("Корова - косарям: дайте сена.");
+    }
+}
+
+class Mower extends Helper {
+    @Override
+    void sendMessage() {
+        System.out.println("Косари - печке: дай калачей.");
+    }
+}
+
+class Stove extends Helper {
+    @Override
+    void sendMessage() {
+        System.out.println("Печка - дровосеку: дай дров.");
+    }
+}
