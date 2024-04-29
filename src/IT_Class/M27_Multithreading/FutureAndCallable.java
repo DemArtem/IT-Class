0dedc795d6e181b63aa54e294668c2dbcf9165e5
gk <demar.demidov@yandex.ru>
2024-04-29 21:28:05 +0300

    test

---
+++ src/IT_Class/M27_Multithreading/FutureAndCallable.java
@@ -0,0 +1,35 @@
+package IT_Class.M27_Multithreading;
+
+import java.util.concurrent.*;
+import java.util.stream.Stream;
+
+public class FutureAndCallable {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //Напоминает runnable, но в отличие от него:
+        //возвращает результат,
+        //выбрасывает исключение (поэтому не надо писать его обработку)
+        Callable<String> callable = getCallable();
+
+        FutureTask<String> future = new FutureTask<>(callable);
+        new Thread(future).start();
+        System.out.println(future.get());
+        System.out.println(Thread.currentThread().getName()+" is finished");
+    }
+
+    static Callable<String> getCallable() {
+        Callable<String> callable = () -> {
+            Integer sum = Stream.iterate(0, i -> i+1)
+                    .limit(10)
+                    .map(p->{
+                        try {
+                            Thread.sleep(10);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return p;})
+                    .reduce(0, Integer:: sum);
+            return Thread.currentThread().getName()+" is finished; sum = " + sum;
+        };
+        return callable;
+    }
+}
---
+++ src/IT_Class/M27_Multithreading/Multicore.java
@@ -0,0 +1,69 @@
+package IT_Class.M27_Multithreading;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Multicore {
+    public static void main(String[] args) throws Exception {
+        //Число доступных ядер (или процессоров)
+        int numCores = Runtime.getRuntime().availableProcessors();
+
+        //System.out.println(numCores);
+
+        //Multi core
+        //Start clock
+        Instant now = Instant.now();
+        //Uses number of available processors
+        ExecutorService executorService = Executors.newWorkStealingPool();
+
+        List<Future<Double>> futures = new LinkedList<>();
+        int numWorkers = 10;
+        for (int i = 0; i < numWorkers; i++) {
+            futures.add(i, executorService.submit(new Worker(i)));
+        }
+        Double avg = 0.0;
+        for (int i = 0; i < numWorkers; i++)
+            avg += futures.get(i).get();
+        Duration duration = Duration.between(now, Instant.now());
+        System.out.printf(" Average: %2.3f, duration: %s%n", avg / numWorkers, duration);
+
+        //Single core
+        now = Instant.now();
+        avg = 0.0;
+        for (int i = 0; i < numWorkers; i++){
+            Worker worker = new Worker(i);
+
+            avg+=worker.call();
+        }
+        duration = Duration.between(now, Instant.now());
+        System.out.printf(" Average: %2.3f, duration: %s%n", avg / numWorkers, duration);
+    }
+}
+
+class Worker implements Callable<Double> {
+    int id;
+    long count = 2 << 20; //Количество добавляемых данных
+    List<Double> data = new ArrayList<>();
+
+    public Worker(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public Double call() throws Exception {
+        for (long i = 0; i < count; i++)
+            data.add(Math.random());
+        Double avg = data.stream()
+                        .mapToDouble(Double::doubleValue)
+                        .summaryStatistics()
+                        .getAverage();
+        return avg;
+    }
+}
